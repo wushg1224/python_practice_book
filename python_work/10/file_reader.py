ce7f6cb5ec20f7e2c4a22d58eca87d6d5459cdbb
Ceci <165979159+wushg1224@users.noreply.github.com>
2024-05-03 00:27:16 +1000

    add chapter 10
    Learn "with open" "var.read()" 'var.rstrip()'

---
+++ python_work/10/file_reader.py
@@ -0,0 +1,16 @@
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+    print(contents)
+    print(contents.rstrip())
+
+
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line)
+
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
